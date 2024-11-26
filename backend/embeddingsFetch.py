792db287356d45b79a3fe53772b74af9508c1610
Manasi Vispute <manasi@10-17-68-124.dynapool.wireless.nyu.edu>
2024-11-26 16:30:25 -0500

    Prompt update

--- backend/embeddingsFetch.py
+++ backend/embeddingsFetch.py
@@ -174,17 +174,17 @@ async def generate_prompt(query: MovieQuery):
     # Create prompt for Flux API
     if query.style == "Illustration (Animated)":
         print("Selected Style:", query.style)
-        prompt = f"Create an image (no text) for the poster for a movie with this plot: {query.plot}. The top 5 closest movies are {top_movies_description}. Generate a poster that is in a flat-image illustration style."
+        prompt = f"Create an image (no text) for the poster for a movie with this plot: {query.plot}. The top 5 closest movies are {top_movies_description}. Generate a poster that is in a flat-image illustration style.The text '{query.title}' must be clearly visible as the title."
         
         # if we manage to train and fine tune our own image generator on the posters we have in our db, the below command might produce better results 
         # prompt = f"Create a poster thats closest to the posters for these movies: {top_movies_description}. Generate a poster that is in a style of flat-image illustration style. The text {query.title} must be clearly visible as the title."
     elif query.style == "Realistic Photography":
         print("Selected Style:", query.style)
-        prompt = f"Create an image (no text) that prominently features a close-up of the face of main subject for the poster for a movie with this plot: {query.plot}. The top 5 closest movies are {top_movies_description}. Generate a poster that stylistically resembles that of the similar movies."
+        prompt = f"Create an image (no text) that prominently features a close-up of the face of main subject for the poster for a movie with this plot: {query.plot}. The top 5 closest movies are {top_movies_description}. Generate a poster that stylistically resembles that of the similar movies.The text '{query.title}' must be clearly visible as the title."
         
     else:
         print("Selected Style:", query.style)
-        prompt = f"Create an image (no text) for the poster for a movie with this plot: {query.plot}. The top 5 closest movies are {top_movies_description}. Generate a poster that stylistically resembles that of the similar movies."
+        prompt = f"Create an image (no text) for the poster for a movie with this plot: {query.plot}. The top 5 closest movies are {top_movies_description}. Generate a poster that stylistically resembles that of the similar movies.The text '{query.title}' must be clearly visible as the title."
         
         # if we manage to train and fine tune our own image generator on the posters we have in our db, the below command might produce better results
         # prompt = f"Create a poster that's closest to the posters for these movies: {top_movies_description}. The text '{query.title}' must be clearly visible as the title."
